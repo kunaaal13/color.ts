2059702f872af88baa5e607972b12534fd831d5d
loki13 <loki@uixlabs.in>
2025-04-05 00:03:00 +0530

    Enhance Color class by adding a private property to store color in RGBA format for improved color management

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { RGB } from './types';
 import { rgbToHex } from './utils/hex';
+
 class Color {
   // Private property to store the color: RGBA -> Colors will be stored as RGBA and converted to other formats when needed
   private color: RGB;
